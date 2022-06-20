9d73e1c50430fbc0f3a63246be09c0ff8cedcb2f
laith <laithalwani@gmail.com>
2022-06-20 00:13:52 -0400

    adds about component, minor adjusemnts to services styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
-
+import About from "./components/About";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Layout>
-          <div className="about"></div>
-          <div className="services" id="services">
-            <Services />
-          </div>
-          <div className="contact" id="contact">
-            <Contact />
-          </div>
-        </Layout>
-      </div>
-    </Router>
+    <div className="App">
+      <Layout>
+        <div className="about">
+          <About />
+        </div>
+        <div className="services" id="services">
+          <Services />
+        </div>
+        <div className="contact" id="contact">
+          <Contact />
+        </div>
+      </Layout>
+    </div>
   );
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h2>About:</h2>
+      <p>
+        Health Heaven Aesthetics is Ottawa’s premier laser clinic, providing
+        aesthetic laser treatments, for over 15 years. Our technicians are
+        trained in the latest techniques and use the latest equipment to have
+        you looking and feeling your best.
+      </p>
+    </div>
+  );
+}

20f26e0791e63028e5307c98ef3415e7d66ed608
laith <laithalwani@gmail.com>
2022-06-22 09:05:42 -0400

    adds active class to nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function Navbar() {
+  const hash = window.location.hash.split("#")
+  const [path, setPath] = useState(hash[1]);
+  
+
+  useEffect(()=>{
+    console.log(path[1])
+  },[path])
   return (
     <nav>
       <div className="logo">
         LaserClinic
       </div>
       <div className="links">
-        <a href="#about">About</a>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact</a>
+        <a href="#about" className={path === "about"? "active":""} onClick={()=>setPath("about")}>About</a>
+        <a href="#services" className={path === "services"? "active":"" } onClick={()=>setPath("services")} >Services</a>
+        <a href="#contact" className={path === "contact"? "active":""} onClick={()=>setPath("contact")}>Contact</a>
       </div>
     </nav>
   );

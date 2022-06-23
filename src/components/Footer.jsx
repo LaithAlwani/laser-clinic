2036499cc8093bfd907d6e96e7d07e9a3ca664f8
Laith Alwani <laithalwani@gmail.com>
2022-06-23 12:52:20 -0400

    adds social media icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import {FaInstagram} from "react-icons/fa";
+import {FaFacebookSquare} from "react-icons/fa";
+import {FaFacebookF} from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer>
+      <div className="social-links">
+        <a href="https://www.facebook.com/AlwaniLaith" target="_blank" rel="noreferrer noopener">
+          {<FaFacebookF />}
+        </a>
+        <a href="https://www.instagram.com/laitho15/?hl=en" target="_blank" rel="noreferrer noopener">
+          {<FaInstagram />}
+        </a>
+      </div>
       <span>copyright Health Haven "email@something.com"</span>
     </footer>
-  )
+  );
 }

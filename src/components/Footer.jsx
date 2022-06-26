8efbc50cc36919f01306a8613288184d33051f70
laith <laithalwani@gmail.com>
2022-06-25 21:35:47 -0400

    adds nav links for mobile navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,13 @@ export default function Footer() {
     <footer>
       <div className="social-links">
         <a href="https://www.facebook.com/AlwaniLaith" target="_blank" rel="noreferrer noopener">
-          {<FaFacebookF />}
+          {<FaFacebookSquare />}
         </a>
         <a href="https://www.instagram.com/laitho15/?hl=en" target="_blank" rel="noreferrer noopener">
           {<FaInstagram />}
         </a>
       </div>
-      <span>copyright Health Haven "email@something.com"</span>
+      <span>Health Haven Asthetics &copy; 2022</span>
     </footer>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {MdMenu} from "react-icons/md"
 
 export default function Navbar() {
   const hash = window.location.hash.split("#")
   const [path, setPath] = useState(hash[1] || null) ;
-  
 
-  useEffect(()=>{
-    
-  },[])
   return (
     <nav>
       <div className="logo">
@@ -25,14 +21,29 @@ export default function Navbar() {
 }
 
 const MobileNav = () => {
+  const hash = window.location.hash.split("#")
+  const [path, setPath] = useState(hash[1] || null) ;
   const [isOpen, setIsOpen] = useState(false)
+  
   const toggleMenu = () => {
     setIsOpen(!isOpen)
   }
+
+  const handleClick = (link)=>{
+    setPath(link)
+    toggleMenu()
+  }
   return (
     <div className="mobile-nav">
-      <span onClick={toggleMenu}><MdMenu size={25} /></span>
+      <span onClick={toggleMenu}><MdMenu size={25} style={{marginRight:"auto"}} /></span>
+      {isOpen && 
+        <>
+        
+        <a href="#about" className={path === "about"? "active":""} onClick={()=>handleClick("about")}>About</a>
+        <a href="#services" className={path === "services"? "active":"" } onClick={()=>handleClick("services")} >Services</a>
+        <a href="#contact" className={path === "contact"? "active":""} onClick={()=>handleClick("contact")}>Contact</a>
       
+        </>}
     </div>
   )
 }
